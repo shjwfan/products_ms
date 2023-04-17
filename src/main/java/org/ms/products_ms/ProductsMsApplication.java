a1ff4d9dcb731fa0a01c516c14041732de309ce6
nocontrib <nocontrib>
2023-04-17 22:37:37 +0600

    run as eureka client

--- src/main/java/org/ms/products_ms/ProductsMsApplication.java
+++ src/main/java/org/ms/products_ms/ProductsMsApplication.java
@@ -2,10 +2,14 @@ package org.ms.products_ms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @EnableAutoConfiguration
+@EnableDiscoveryClient
 @ComponentScan("org.ms.products_ms")
+@Configuration
 public class ProductsMsApplication {
 
   public static void main(String[] args) {
